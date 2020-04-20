c43773c32f613a27e9c51cc81c83ea1814929c7f
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2020-04-20 14:05:50 +0100

    updated cv pages

--- js/flip-card.js
+++ js/flip-card.js
@@ -2,3 +2,15 @@ var card = document.querySelector('.card');
 card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
 });
+
+var pat = document.querySelector('.card-pat');
+pat.addEventListener( 'click', function() {
+  pat.classList.toggle('is-flipped');
+  console.log('flipped');
+});
+
+var college = document.querySelector('.card-college');
+college.addEventListener( 'click', function() {
+  college.classList.toggle('is-flipped');
+  console.log('flipped');
+});
